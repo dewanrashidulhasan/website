3c714fe12feaff182cb3dd53dd381cf9bce6653e
Dewan Rashidul Hasan <drhz671@gmail.com>
2024-10-12 17:45:39 +0600

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,22 +6,22 @@ const Navbar = () => {
     <div className=' flex item-center justify-between py-4 font-medium bg-slate-500 w-full' >
     
     <img src={assets.logo} className='w-46 h-10' alt="logo" />
-      <ul className='hidden sm:flex gap-5 text-sm text-gray-700 px-4'>
-        <NavLink to='/home' className='flex flex-col items-center gap1'>
+      <ul className='hidden sm:flex gap-5 text-sm text-white px-4'>
+        <NavLink to='/home' className='flex flex-col items-center gap-1'>
             <p>Home</p>
-            <hr className='w-2/4 border-none h-[1.5px] bg-gray-700' />
+            <hr className='w-2/4 border-none h-[1.5px] bg-white' />
         </NavLink>
-        <NavLink to='/collection' className='flex flex-col items-center gap1'>
+        <NavLink to='/collection' className='flex flex-col items-center gap-1'>
             <p>COLLECTION</p>
-            <hr className='w-2/4 border-none h-[1.5px] bg-gray-700' />
+            <hr className='w-2/4 border-none h-[1.5px] bg-white' />
         </NavLink>
-        <NavLink to='/about' className='flex flex-col items-center gap1'>
+        <NavLink to='/about' className='flex flex-col items-center gap-1'>
             <p>ABOUT</p>
-            <hr className='w-2/4 border-none h-[1.5px] bg-gray-700' />
+            <hr className='w-2/4 border-none h-[1.5px] bg-white' />
         </NavLink>
-        <NavLink to='/contact' className='flex flex-col items-center gap1'>
+        <NavLink to='/contact' className='flex flex-col items-center gap-1'>
             <p>CONTACT</p>
-            <hr className='w-2/4 border-none h-[1.5px] bg-gray-700' />
+            <hr className='w-2/4 border-none h-[1.5px] bg-white' />
         </NavLink>
 
       </ul>
