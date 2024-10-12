bf255efe4026f5fd8adf553cb6875e45ae765b9c
Dewan Rashidul Hasan <drhz671@gmail.com>
2024-10-13 00:29:57 +0600

    shopContext

---
+++ src/components/LatestCollection.jsx
@@ -0,0 +1,13 @@
+import React, { useContext } from 'react'
+import {Shopcontext} from '../context/ShopContext'
+const LatestCollection = () => {
+
+    const { products } = useContext(Shopcontext);
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default LatestCollection
