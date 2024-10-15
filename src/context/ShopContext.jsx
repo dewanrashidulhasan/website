88ee2519f7a9b585fd322bce8f0ae8fee983302d
Dewan Rashidul Hasan <drhz671@gmail.com>
2024-10-16 00:11:01 +0600

    products cart

--- src/context/ShopContext.jsx
+++ src/context/ShopContext.jsx
@@ -1,5 +1,4 @@
-
-import { createContext, useState } from "react";
+import React, { createContext, useEffect, useState } from "react";
 import { products } from "../assets/assets";
 
 export const ShopContext = createContext();
@@ -8,7 +7,29 @@ const ShopContextProvider = (props) => {
     const currency = '$';
     const delivery_fee = 10; // Spelling corrected from 'delevery_fee' to 'delivery_fee'
     const [search, setSearch] = useState('');
-    const [showSearch, setShowSearch] = useState(false); // Default false to hide the search bar
+    const [showSearch, setShowSearch] = useState(false); 
+    const [cartItems, setCartItems] = useState({});
+
+    const addToCart = async (itemId, size) => { // Fixed typo in function name
+
+        let cartData = structuredClone(cartItems);
+
+        if (cartData[itemId]) {
+            if (cartData[itemId][size]) {
+                cartData[itemId][size] += 1;
+            } else {
+                cartData[itemId][size] = 1;
+            }
+        } else {
+            cartData[itemId] = {};
+            cartData[itemId][size] = 1;
+        }
+        setCartItems(cartData);
+    };
+
+    useEffect(() => {
+        console.log(cartItems);
+    }, [cartItems]);
 
     const value = {
         products,
@@ -17,7 +38,9 @@ const ShopContextProvider = (props) => {
         search,
         setSearch,
         showSearch,
-        setShowSearch
+        setShowSearch,
+        cartItems,
+        addToCart, // Fixed typo in value object
     };
 
     return (
--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -6,11 +6,11 @@ import RelatedProducts from '../components/RelatedProducts';
 
 const Product = () => {
     const { productId } = useParams();
-    const { products, currency } = useContext(ShopContext);
+    const { products, currency, addToCart } = useContext(ShopContext);
     const [images, setImages] = useState([]);
     const [foundProduct, setFoundProduct] = useState(null);
     const [image, setImage] = useState('');
-    const [selectedSize, setSelectedSize] = useState('');
+    const [selectedSize, setSelectedSize] = useState(''); // Fixed hook state definition
 
     useEffect(() => {
         const fetchProductData = () => {
@@ -93,7 +93,7 @@ const Product = () => {
                         </div>
                         <button 
                             className="bg-gray-800 text-white font-semibold py-2 px-2 rounded-lg shadow-md transition duration-300 ease-in-out transform hover:bg-blue-700 hover:scale-105 focus:outline-none focus:ring-2 focus:ring-blue-300"
-                            onClick={() => console.log('Added to cart!')} 
+                            onClick={() => addToCart(foundProduct._id, selectedSize)} // Fixed parameter name
                         >
                             ADD TO CART
                         </button>
